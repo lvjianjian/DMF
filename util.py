37be35141bb09bad465ecb6e41a58d84dc64c404
Zhongjian Lv <568066242@qq.com>
2018-11-09 00:44:32 +0800

    add ingxing

--- util.py
+++ util.py
@@ -546,7 +546,7 @@ def encode_vt(train_df, test_df, variable, target, use_bayes=True):
     return df
 
 
-def transform(id_cate, target, train, test, use_bayes_smooth=True):
+def transform(id_cate, target, train, test, use_bayes_smooth=True, bayes_n_split=5):
     if (type(id_cate) != list):
         id_cate = [id_cate]
     print("%s unique num: %s" % (id_cate, train[id_cate].nunique()))
@@ -554,7 +554,7 @@ def transform(id_cate, target, train, test, use_bayes_smooth=True):
     col_name = col_name + "_" + target + "_ctr"
     bayes_feature = encode_vt(train, test, id_cate, target, use_bayes_smooth)
     test[col_name] = bayes_feature
-    skf = StratifiedKFold(n_splits=5, shuffle=True, random_state=2018)
+    skf = StratifiedKFold(n_splits=bayes_n_split, shuffle=True, random_state=2018)
     for i, (train_idx, test_idx) in enumerate(skf.split(np.zeros(len(train)), train[target])):
         print(id_cate, target, i)
         X_train = train.iloc[train_idx]
