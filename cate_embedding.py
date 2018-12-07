726af485ec96492eabf4f84d53744b5914c17e1e
Zhongjian Lv <568066242@qq.com>
2018-12-07 16:00:40 +0800

    fix some

--- cate_embedding.py
+++ cate_embedding.py
@@ -76,13 +76,14 @@ def decomposition_ipca(dataPath, origin_pickle_name, pca_length, decom_col_name=
         f = newdf
     return f
 
-def simple_countVectorizer(df, dataPath, cate1, cate2, min_df=2, to_tfidf=False):
+def simple_countVectorizer(df, dataPath, cate1, cate2, min_df=2, to_tfidf=False,ngram_range=(1,1)):
     if not os.path.exists(os.path.join(dataPath, 'cache/')):
         os.mkdir(os.path.join(dataPath, 'cache/'))
     sentence_file = os.path.join(dataPath,
-                                 'cache/%s_%s_mindf_%d_Simple_CountVectorizer_vector.npz' % (cate1, cate2, min_df))
+                                 'cache/%s_%s_mindf_%d_Simple_CountVectorizer_vector_ngram_%s.npz' % (cate1, cate2, min_df,  '{}_{}'.format(ngram_range[0], ngram_range[1])))
     cate1s_file = os.path.join(dataPath,
-                               'cache/%s_%s_mindf_%d_Simple_CountVectorizer_cate1s.npz' % (cate1, cate2, min_df))
+                               'cache/%s_%s_mindf_%d_Simple_CountVectorizer_cate1s_ngram_%s.npz' % (cate1, cate2, min_df,  '{}_{}'.format(ngram_range[0], ngram_range[1])))
+
     if (not os.path.exists(sentence_file)) or (not os.path.exists(cate1s_file)):
         # 保证文件内容的是一致的 不能有错位
         if os.path.exists(sentence_file):
@@ -99,7 +100,7 @@ def simple_countVectorizer(df, dataPath, cate1, cate2, min_df=2, to_tfidf=False)
         gc.collect()
 
         cate2_as_matrix = CountVectorizer(token_pattern='(?u)\\b\\w+\\b',
-                                          min_df=min_df).fit_transform(cate2_as_sentence)
+                                          min_df=min_df,ngram_range=ngram_range).fit_transform(cate2_as_sentence)
         scipy.sparse.save_npz(sentence_file, cate2_as_matrix)
         np.savez(cate1s_file, cate1s=cate1s)
     else:
@@ -387,25 +388,26 @@ class CateEmbedding(object):
         interaction = pd.merge(interaction, cate2_df, on=cate2, how="outer")
         return interaction[self.MAIN_ID + p_cols]
 
-    def _embedding(self, dataPath, df, cate1, cate2, method, method_name, n_components=16, min_df=2, to_tfidf=False):
+    def _embedding(self, dataPath, df, cate1, cate2, method, method_name, n_components=16, min_df=2, to_tfidf=False, ngram_range=(1,1)):
         tfidf_name = ""
         if(to_tfidf):
             tfidf_name = "_tfidf"
         embedding_query_file = os.path.join(dataPath,
-                                            'cache/%s_%s_nc_%d_mindf_%d_%s_embedding%s.feather' % (
-                                                cate1, cate2, n_components, min_df, method_name, tfidf_name))
+                                            'cache/%s_%s_nc_%d_mindf_%d_%s_embedding%s_ngram_%s.feather' % (
+                                                cate1, cate2, n_components, min_df, method_name, tfidf_name, '{}_{}'.format(ngram_range[0], ngram_range[1])))
 
         if not os.path.exists(embedding_query_file):
             if not os.path.exists(os.path.join(dataPath, 'cache/')):
                 os.mkdir(os.path.join(dataPath, 'cache/'))
 
-            cate1s, cate2_as_matrix = simple_countVectorizer(df, dataPath, cate1, cate2, min_df=min_df, to_tfidf=to_tfidf)
+            cate1s, cate2_as_matrix = simple_countVectorizer(df, dataPath, cate1, cate2, min_df=min_df, to_tfidf=to_tfidf,
+                                                             ngram_range=ngram_range)
             topics_of_cate1 = method.fit_transform(cate2_as_matrix)
             del cate2_as_matrix;
             gc.collect()
 
             topics_of_cate1 = pd.DataFrame(topics_of_cate1,
-                                           columns=["%s_%s_%s_%s%s" % (cate1, cate2, i, method_name, tfidf_name) for i in
+                                           columns=["%s_%s_%s_%s%s_ngram_%s" % (cate1, cate2, i, method_name, tfidf_name, '{}_{}'.format(ngram_range[0], ngram_range[1])) for i in
                                                     range(n_components)]).astype('float32')
 
             topics_of_cate1[cate1] = cate1s
@@ -420,7 +422,7 @@ class CateEmbedding(object):
             topics_of_cate1 = pd.read_feather(embedding_query_file)
             return topics_of_cate1
 
-    def lda_embedding(self, dataPath, df, cate1, cate2, n_components=16, min_df=2, batch_size=520, n_jobs=20, to_tfidf=False):
+    def lda_embedding(self, dataPath, df, cate1, cate2, n_components=16, min_df=2, batch_size=520, n_jobs=20, to_tfidf=False, ngram_range = (1,1)):
         '''
         此部分是做cate1 cate2的相关embedding,这里只要共同show过的都算相关
         '''
@@ -431,10 +433,10 @@ class CateEmbedding(object):
                                         n_jobs=n_jobs
                                         )
 
-        return self._embedding(dataPath, df, cate1, cate2, lda, "lda", n_components=n_components, min_df=min_df,to_tfidf=to_tfidf)
+        return self._embedding(dataPath, df, cate1, cate2, lda, "lda", n_components=n_components, min_df=min_df,to_tfidf=to_tfidf, ngram_range=ngram_range)
 
     def nmf_embedding(self, dataPath, df, cate1, cate2, n_components=16, min_df=2,
-                      max_iter=1000, alpha=.1, l1_ratio=.5, to_tfidf=False):
+                      max_iter=1000, alpha=.1, l1_ratio=.5, to_tfidf=False, ngram_range = (1,1)):
         nmf = NMF(n_components=n_components,
                   random_state=2018,
                   beta_loss='kullback-leibler',
@@ -443,11 +445,11 @@ class CateEmbedding(object):
                   alpha=alpha,
                   l1_ratio=l1_ratio)
 
-        return self._embedding(dataPath, df, cate1, cate2, nmf, "nmf", n_components=n_components, min_df=min_df, to_tfidf=to_tfidf)
+        return self._embedding(dataPath, df, cate1, cate2, nmf, "nmf", n_components=n_components, min_df=min_df, to_tfidf=to_tfidf, ngram_range=ngram_range)
 
-    def svd_embedding(self, dataPath, df, cate1, cate2, n_components=16, min_df=2, tol=0., n_iter=5, to_tfidf=False):
+    def svd_embedding(self, dataPath, df, cate1, cate2, n_components=16, min_df=2, tol=0., n_iter=5, to_tfidf=False, ngram_range = (1,1)):
         svd = TruncatedSVD(n_components, random_state=2018, tol=tol, n_iter=n_iter)
-        return self._embedding(dataPath, df, cate1, cate2, svd, "svd", n_components=n_components, min_df=min_df, to_tfidf=to_tfidf)
+        return self._embedding(dataPath, df, cate1, cate2, svd, "svd", n_components=n_components, min_df=min_df, to_tfidf=to_tfidf, ngram_range=ngram_range)
 
 
         # nn自编码输出编码层
